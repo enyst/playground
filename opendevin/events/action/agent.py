sm-Hassan-Kazmi/OpenDevin

add delegate summaries

opendevin/events/action/agent.py
@@ -46,7 +46,6 @@ class AgentSummarizeAction(Action):
     action: str = ActionType.SUMMARIZE
     _chunk_start: int = -1
     _chunk_end: int = -1
-    is_delegate_summary: bool = False
 
     @property
     def message(self) -> str:
@@ -54,6 +53,34 @@ class AgentSummarizeAction(Action):
 
     def __str__(self) -> str:
         ret = '**AgentSummarizeAction**\n'
+        ret += f'SUMMARY: {self.summary}'
+        return ret
+
+
+@dataclass
+class AgentDelegateSummaryAction(Action):
+    """
+    Action to summarize a list of events.
+
+    Attributes:
+    - agent: What agent was delegated to.
+    - task: The task that was delegated.
+    - summary: A single sentence summarizing all the delegate's actions and observations.
+    """
+
+    agent: str = ''
+    task: str = ''
+    summary: str = ''
+    action: str = ActionType.SUMMARIZE
+    _chunk_start: int = -1
+    _chunk_end: int = -1
+
+    @property
+    def message(self) -> str:
+        return self.summary
+
+    def __str__(self) -> str:
+        ret = '**AgentDelegateSummaryAction**\n'
         ret += f'SUMMARY: {self.summary}'
         return ret
 

enyst/playground

opendevin/events/action/agent.py
@@ -47,6 +47,35 @@ class AgentSummarizeAction(Action):
 
 
 @dataclass
+class AgentDelegateSummaryAction(Action):
+    """
+    Action to summarize a list of events.
+
+    Attributes:
+    - agent: What agent was delegated to.
+    - task: The task that was delegated.
+    - summary: A single sentence summarizing all the delegate's actions and observations.
+    """
+
+    agent: str = ''
+    task: str = ''
+    summary: str = ''
+    relevant_info: str = ''
+    action: str = ActionType.SUMMARIZE_DELEGATE
+    _chunk_start: int = -1
+    _chunk_end: int = -1
+
+    @property
+    def message(self) -> str:
+        return self.summary
+
+    def __str__(self) -> str:
+        ret = '**AgentDelegateSummaryAction**\n'
+        ret += f'SUMMARY: {self.summary}'
+        return ret
+
+
+@dataclass
 class AgentFinishAction(Action):
     outputs: dict = field(default_factory=dict)
     thought: str = ''
@@ -54,7 +83,7 @@ class AgentFinishAction(Action):
 
     @property
     def message(self) -> str:
-        if self.thought != "":
+        if self.thought != '':
             return self.thought
         return "All done! What's next on the agenda?"
 